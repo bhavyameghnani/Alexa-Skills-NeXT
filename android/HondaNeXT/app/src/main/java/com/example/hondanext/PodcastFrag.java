8144e439484dfbd7818b2f3a3819e3c24c5f7cf7
aakankshaa23 <taakanksha99@gmail.com>
2020-12-24 13:39:49 +0530

    android added

---
+++ android/HondaNeXT/app/src/main/java/com/example/hondanext/PodcastFrag.java
@@ -0,0 +1,28 @@
+package com.example.hondanext;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class PodcastFrag extends Fragment {
+
+
+
+    public PodcastFrag() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_podcast, container, false);
+    }
+}
